3c925489e348e8d38af1bd78d0e2e3404de7c177
Marc02130 <marc.breneiser@gmail.com>
2025-01-26 14:07:31 -0500

    release 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Amplify } from 'aws-amplify'
-import config from './amplifyconfiguration.json'
 
-Amplify.configure(config)
+// Configure Amplify directly with environment variables
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+      signUpVerificationMethod: 'code'
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
